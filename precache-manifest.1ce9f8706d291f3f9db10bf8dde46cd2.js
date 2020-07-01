7f71788eb6862e3344f82dd9feee6e9eccb733f6
birbetal <rameshseti775@gmail.com>
2020-07-01 21:08:51 +0545

    Updates

---
+++ precache-manifest.1ce9f8706d291f3f9db10bf8dde46cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469487042a7b7963d3c1a9e81f55417c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "549a3026ec2d0120498d",
+    "url": "/react-gh-pages/static/css/main.7e3dc4e8.chunk.css"
+  },
+  {
+    "revision": "853035305fb6e73ef703",
+    "url": "/react-gh-pages/static/js/2.c95dedfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.c95dedfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549a3026ec2d0120498d",
+    "url": "/react-gh-pages/static/js/main.6393ba6c.chunk.js"
+  },
+  {
+    "revision": "0f412b7d663b59b98489",
+    "url": "/react-gh-pages/static/js/runtime-main.b230fd53.js"
+  }
+]);
